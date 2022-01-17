48a25ae18d3ff1e6fb79c6112a7b2e872c6e322b
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-17 19:45:40 +0000

    Add route handlers for PUT, PATCH and DELETE operations for specific article

--- app.js
+++ app.js
@@ -70,7 +70,52 @@ app.route("/articles")
         res.send("No articles matching that title was found.");
       }
     })
-  });
+  })
+
+.put(function(req, res){
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if(!err){
+        res.send("Successfully updated the selected article.");
+      }
+    }
+  );
+})
+
+.patch(function(req, res){
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+
+.delete(function(req, res){
+
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if (!err){
+        res.send("Successfully deleted the corresponding article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
